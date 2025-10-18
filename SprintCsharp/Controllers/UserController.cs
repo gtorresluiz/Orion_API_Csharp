ad88ed94ac63f9c70ccdfd091b5fd34d9ee66942
gtorresluiz <rm98600@fiap.com.br>
2025-10-18 13:10:17 -0300

    fix: put method

--- SprintCsharp/Controllers/UserController.cs
+++ SprintCsharp/Controllers/UserController.cs
@@ -32,6 +32,33 @@ namespace SprintCsharp.Controllers
             return CreatedAtAction(nameof(Get), new { id = user.Id }, user);
         }
 
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] User updatedUser)
+        {
+            // Busca o usuário existente
+            var existingUser = _service.GetUserById(id);
+            if (existingUser == null)
+                return NotFound(); // Retorna 404 se não existir
+
+            // Atualiza os campos do usuário existente
+            existingUser.Id = existingUser.Id;
+            existingUser.Name = updatedUser.Name;
+            existingUser.Email = updatedUser.Email;
+            existingUser.Balance = updatedUser.Balance;
+            existingUser.PreferredInvestment = updatedUser.PreferredInvestment;
+            existingUser.Level = updatedUser.Level;
+            existingUser.UpdatedAt = updatedUser.UpdatedAt;
+
+            
+            // Adicione aqui outros campos que devem ser atualizados
+
+            // Salva as alterações
+            _service.UpdateUser(existingUser);
+
+            return NoContent(); // 204 No Content para indicar sucesso
+        }
+
+
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
